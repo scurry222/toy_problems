06ce3815384aa237761466b1f2bf4c3bf04889a5
scurry222 <scurry222@gmail.com>
2021-03-30 12:38:43 -0600

    Add rotate array to the right in place: time O(n) space O(1)

---
+++ RotateArray/rotate.js
@@ -0,0 +1,60 @@
+// Rotate Array
+
+// Solution
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+// Example 2:
+
+// Input: nums = [-1,-100,3,99], k = 2
+// Output: [3,99,-1,-100]
+// Explanation: 
+// rotate 1 steps to the right: [99,-1,-100,3]
+// rotate 2 steps to the right: [3,99,-1,-100]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 2 * 104
+// -231 <= nums[i] <= 231 - 1
+// 0 <= k <= 105
+ 
+
+// Follow up:
+
+// Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+// Could you do it in-place with O(1) extra space?
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ var rotate = function(nums, k) {
+    nums.reverse();
+    for (let i = 0; i < k; i++) {
+        let swap = nums.shift();
+        nums.push(swap);
+    }
+    nums.reverse();
+};
+
+// var findGCD = function(len, k) {
+//     return len === 0
+//     ? k : k === 0
+//     ? len : len === k
+//     ? len : len > k
+//     ? findGCD(len-k, k) : findGCD(len, k-len);
+// }
+
+// console.log(rotate([1,2,3,4,5,6,7], 3));
+
